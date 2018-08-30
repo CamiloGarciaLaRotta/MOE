60b1f0b37202c1f679e75264c8ba3d0e1d7f17e9
cegal <camilo.garcia.larotta@gmail.com>
2018-08-30 11:49:03 -0400

    Add Unicode separator and default characters to encoder
    Closes #32

--- moe/encoder.py
+++ moe/encoder.py
@@ -7,12 +7,14 @@ class Encoder():
 
     Args:
         file (str): The csv file containing the encoding: LETTER, CODE.
-            It can have an entry for default space called SPACE.
+            It can have an entry for default space called SPACE. i.e. the characte to separate encoded words
+            It can have an entry for default separator called SEPARATOR. i.e. the character to separate encoded letters
             It can have an entry for default non supported values called DEFAULT.
         default_space (str, optional): Defaults to ' '. If no default space is present in the CSV file, it can be passed here.
-        default_value (str, optional): Defaults to 'X'. If no default value is present in the CSV file, it can be passed here.'''
+        default_separator (str, optional): Defaults to '¶'. If no default space is present in the CSV file, it can be passed here.
+        default_value (str, optional): Defaults to '⁜'. If no default value is present in the CSV file, it can be passed here.'''
 
-    def __init__(self, file: str, default_space: str = ' ', default_value: str = 'X') -> None:
+    def __init__(self, file: str, default_space: str = ' ', default_separator: str = '¶', default_value: str = '⁜') -> None:
 
         self.dictionnary, self.reverse_dictonnary = {}, {}
         with open(file, mode='r') as csv_file:
@@ -24,9 +26,11 @@ class Encoder():
                 line_count += 1
             print(f'Processed {line_count} lines.')
 
+        if 'SEPARATOR' not in self.dictionnary.keys():
+            self.dictionnary['SEPARATOR'] = default_separator
+
         if 'SPACE' not in self.dictionnary.keys():
             self.dictionnary['SPACE'] = default_space
-            self.reverse_dictonnary['SPACE'] = default_space
 
         if 'DEFAULT' not in self.dictionnary.keys():
             self.dictionnary['DEFAULT'] = default_value
@@ -50,9 +54,9 @@ class Encoder():
             else:
                 coded_text.append(self.dictionnary['DEFAULT'])
 
-            coded_text.append('¶')
+            coded_text.append(self.dictionnary['SEPARATOR'])
 
-        return ''.join(coded_text).rstrip('¶')
+        return ''.join(coded_text).rstrip(self.dictionnary['SEPARATOR'])
 
     def decode(self, coded_string: str) -> str:
         '''Decode a given coded string.
@@ -64,9 +68,9 @@ class Encoder():
             str: The string containing the equivalent text.'''
 
         decoded_text = []
-        for code in coded_string.rstrip().split('¶'):
-            if code.isspace():
-                decoded_text.append(self.reverse_dictonnary['SPACE'])
+        for code in coded_string.rstrip().split(self.dictionnary['SEPARATOR']):
+            if code == self.dictionnary['SPACE']:
+                decoded_text.append(' ')
             elif code in self.reverse_dictonnary.keys():
                 decoded_text.append(self.reverse_dictonnary[code])
             else:
--- tests/unit/test_encoder.py
+++ tests/unit/test_encoder.py
@@ -7,7 +7,6 @@ from moe.encoder import Encoder
 WORDS = (('HELLO', '....¶.¶._..¶._..¶___'),
          ('S P A C E', '...¶ ¶.__.¶ ¶._¶ ¶_._.¶ ¶.'),
          ('NEPE', '_.¶.¶.__.¶.'),
-         ('I LOVE YOU', '..¶ ¶._..¶___¶..._¶.¶ ¶_.__¶___¶.._'),
          ('0123THEANSWERIS42', '_____¶.____¶..___¶...__¶_¶....¶.¶._¶_.¶...¶.__¶.¶._.¶..¶...¶...._¶..___'))
 
 
@@ -34,3 +33,15 @@ def test_decoder(morser):
 
     for expected, code in WORDS:
         assert expected == morser.decode(code)
+
+
+def test_default_character(morser):
+    '''GIVEN a word containing unsupported characters
+    WHEN the word is encoded/decoded
+    THEN it will be filled with the default character ⁜'''
+
+    original, encoded, decoded = 'I ℇ⇋⊙⛄ YOU', '..¶ ¶⁜¶⁜¶⁜¶⁜¶ ¶_.__¶___¶.._', 'I ⁜⁜⁜⁜ YOU'
+
+    actual_encoded = morser.encode(original)
+    assert encoded == actual_encoded
+    assert decoded == morser.decode(actual_encoded)
